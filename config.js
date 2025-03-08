1ed72ffe89a1d90fc545357e50fb409d19493790
aekawat <107487345+aekawat@users.noreply.github.com>
2025-03-08 20:53:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbyShDws_wiD4ntl-OOMQMy9rBNLm_yc-90YVvoikDYsNAm4LJI_jQCU96GMnugusIBR1g/exec"
 }
